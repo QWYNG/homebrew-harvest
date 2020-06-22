aed42e6b6866460d09369bfd4399c6d793d223d7
Hiroaki Osawa <ikusawasi@gmail.com>
2020-06-22 22:33:44 +0900

    update 0.1.2

--- harvest.rb
+++ harvest.rb
@@ -1,9 +1,9 @@
 class Harvest < Formula
   desc "stashes grep tool with Rust"
   homepage "https://github.com/QWYNG/harvest"
-  url "https://github.com/QWYNG/harvest/releases/download/v0.0.3/harvest-v0.1.1-x86_64-mac.zip"
-  sha256 "530280f60dc992574d00a53e27cf0969da5cf2d3d6aab9fb3f02cf7b1c4deab7"
-  version "0.0.3"
+  url "https://github.com/QWYNG/harvest/releases/download/v0.1.2/harvest-v0.1.2-x86_64-mac.zip"
+  sha256 "f7d3029001f618fbdfb9cdfa97fa5b88718301b13af7fc3f42c20c762e5e130a"
+  version "0.1.2"
 
   def install
     bin.install "harvest"
