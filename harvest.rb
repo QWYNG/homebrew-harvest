be90974d7e1ad3ad25a58ea53d179872bf329483
Hiroaki Osawa <ikusawasi@gmail.com>
2020-07-05 18:08:22 +0900

    Update harvest.rb

--- harvest.rb
+++ harvest.rb
@@ -1,10 +1,10 @@
-require_relative 'version'
+require_relative './version'
 
 class Harvest < Formula
-  version VERSION
+  version ::VERSION
   desc "stashes grep tool with Rust"
   homepage "https://github.com/QWYNG/harvest"
-  url "https://github.com/QWYNG/harvest/releases/download/#{VERSION}/harvest-#{VERSION}-x86_64-mac.zip"
+  url "https://github.com/QWYNG/harvest/releases/download/#{version}/harvest-#{version}-x86_64-mac.zip"
   sha256 "f7d3029001f618fbdfb9cdfa97fa5b88718301b13af7fc3f42c20c762e5e130a"
 
   def install
