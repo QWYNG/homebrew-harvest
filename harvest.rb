e4a9495620fba08fb446a4e3e5ef2e95ca8c8a4f
QWYNG <ikusawasi@gmail.com>
2020-07-05 16:04:15 +0900

    add Release workflow

--- harvest.rb
+++ harvest.rb
@@ -1,9 +1,11 @@
+require_relative 'version'
+
 class Harvest < Formula
+  version VERSION
   desc "stashes grep tool with Rust"
   homepage "https://github.com/QWYNG/harvest"
-  url "https://github.com/QWYNG/harvest/releases/download/v0.1.2/harvest-v0.1.2-x86_64-mac.zip"
+  url "https://github.com/QWYNG/harvest/releases/download/#{version}/harvest-#{version}-x86_64-mac.zip"
   sha256 "f7d3029001f618fbdfb9cdfa97fa5b88718301b13af7fc3f42c20c762e5e130a"
-  version "0.1.2"
 
   def install
     bin.install "harvest"
